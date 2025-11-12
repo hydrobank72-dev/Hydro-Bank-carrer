434d006a038ac9bc42236e681714a51e65b2747d
Dipanshu Rawat <dv451197@gmail.com>
2025-11-12 20:34:09 +0530

    fix: build error while netlify deploy

---
+++ src/assets/img-export.ts
@@ -0,0 +1,9 @@
+import Frontend from "../assets/frontend.jpg";
+import AppDev from "../assets/appdevelopment.jpg";
+import Blockchain from "../assets/blockchain.jpg";
+import AIML from "../assets/ai-ml.jpg";
+import DeepLearning from "../assets/deeplearning.jpg";
+import DataScience from "../assets/data-scientist.jpg";
+import HR from "../assets/hr-team.jpg";
+
+// export default{ Frontend AIML}
